54c2f6dd4c577aecd5dcc7a79a1992766d3084ac
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-10-26 23:38:34 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex sm:min-h-[91vh] min-h-[88vh] flex-col items-center justify-center text-center px-2 py-8">
-      <Link
-        href="https://github.com/nisabmohd/Aria-Docs"
-        target="_blank"
-        className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4"
-      >
-        Follow along on GitHub{" "}
-        <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
-      </Link>
       <h1 className="text-3xl font-bold mb-4 sm:text-7xl">
         PCW Bug Hunters
       </h1>
@@ -41,7 +33,6 @@ export default function Home() {
         </Link>
       </div>
       <span className="flex flex-row items-start sm:gap-2 gap-0.5 text-muted-foreground text-md mt-7 -mb-12 max-[800px]:mb-12 font-code text-base font-medium">
-        <TerminalSquareIcon className="w-5 h-5 mr-1 mt-0.5" />
         {"The truth hides within the code"}
       </span>
     </div>
