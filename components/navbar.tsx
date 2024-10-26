129e108f3cc63881d77db46a248252604368a6c7
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-10-26 23:28:11 +0300

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,16 +18,12 @@ export const NAVLINKS = [
     href: "/blog",
   },
   {
-    title: "Examples",
-    href: "#",
-  },
-  {
     title: "Guides",
     href: "#",
   },
   {
     title: "Community",
-    href: "https://github.com/nisabmohd/Aria-Docs/discussions",
+    href: "https://github.com/ThatSINEWAVE/pcw-bughunters/discussions",
   },
 ];
 
@@ -52,19 +48,10 @@ export function Navbar() {
             <Search />
             <div className="flex ml-2.5 sm:ml-0">
               <Link
-                href="https://github.com/nisabmohd/NexDocs"
+                href="https://github.com/ThatSINEWAVE/pcw-bughunters"
                 className={buttonVariants({ variant: "ghost", size: "icon" })}
               >
                 <GithubIcon className="h-[1.1rem] w-[1.1rem]" />
-              </Link>
-              <Link
-                href="#"
-                className={buttonVariants({
-                  variant: "ghost",
-                  size: "icon",
-                })}
-              >
-                <TwitterIcon className="h-[1.1rem] w-[1.1rem]" />
               </Link>
               <ModeToggle />
             </div>
@@ -79,7 +66,7 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2.5">
       <CommandIcon className="w-6 h-6 text-muted-foreground" strokeWidth={2} />
-      <h2 className="text-md font-bold font-code">AriaDocs</h2>
+      <h2 className="text-md font-bold font-code">PCW Bug Hunters</h2>
     </Link>
   );
 }
