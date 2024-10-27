95336d45fd9c0ad628782be5329792226b168f00
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-10-27 03:38:07 +0200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,14 +9,14 @@ export function Footer() {
         <div className="flex items-center gap-3">
           <CommandIcon className="sm:block hidden w-5 h-5 text-muted-foreground" />
           <p className="text-center">
-            Build by{" "}
+            Build by{""}
             <Link
               className="px-1 underline underline-offset-2"
               href="https://github.com/ThatSINEWAVE"
             >
               SINEWAVE
             </Link>
-            . The source code is available on{" "}
+            . The source code is available on{""}
             <Link
               className="px-1 underline underline-offset-2"
               href="https://github.com/ThatSINEWAVE/pcw-bughunters"
