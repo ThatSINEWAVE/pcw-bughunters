bdd17f18e2e46bfa82356093dcd1fbfe3dd9d234
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-11-10 20:49:27 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,16 @@ export default function Home() {
         >
           Blog
         </Link>
+        <Link
+          href="/news"
+          className={buttonVariants({
+            variant: "secondary",
+            className: "px-6",
+            size: "lg",
+          })}
+        >
+          news
+        </Link>
       </div>
       <span className="flex flex-row items-center sm:gap-2 gap-1 text-muted-foreground text-md mt-7 -mb-12 max-[800px]:mb-12 font-code text-base font-medium">
         <TerminalSquareIcon className="w-5 h-5 mr-2" />
